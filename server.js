377d8c94278bbd38675268c0b1af73b6bbb57c07
beetech <brighteyo16@gmail.com>
2022-12-16 11:14:27 +0100

    removed cors settings

--- server.js
+++ server.js
@@ -12,9 +12,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(bodyParser.json());
-app.use(cors({
-    origin: ["http://localhost:3000", "https://virtualoffice.nriherbal.com/"]
-}))
+app.use(cors())
 
 // Routes Middleware
 app.use('/api/users', userRoute)
